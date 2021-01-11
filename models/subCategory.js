4c42d46ed3dbebc42872422015e6ac67670ed8bf
rishank-shah <shahrishank@gmail.com>
2021-01-11 19:41:11 +0530

    subcategory commit

---
+++ models/subCategory.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const {ObjectId} = mongoose.Schema
+
+const subCategorySchema = new mongoose.Schema({
+    name: {
+        type:String,
+        trim:true,
+        required:'Name is required for category',
+        minlength: [2,'Too short'],
+        maxlength: [20,'Too long']
+    }, 
+    slug:{
+        type:String,
+        unique:true,
+        lowercase:true,
+        index:true
+    },
+    parent:{
+        type:ObjectId,
+        ref:"Category",
+        required:true
+    }
+},
+{
+    timestamps:true
+})
+
+module.exports = mongoose.model('SubCategory',subCategorySchema)
---
+++ routes/subCategory.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+const {create,read,update,remove,list} = require('../controllers/subCategory')
+const {authCheck,adminCheck} = require('../middlewares/auth')
+
+router.get('/subcategory/list',list)
+router.get('/subcategory/:slug',read)
+
+router.post('/subcategory',authCheck,adminCheck,create)
+router.put('/subcategory/:slug',authCheck,adminCheck,update)
+router.delete('/subcategory/:slug',authCheck,adminCheck,remove)
+
+
+module.exports = router;
