aa3fdaff9c6fbfb1e62e71afb5c4a961d4a7e386
rishank-shah <shahrishank@gmail.com>
2021-02-09 14:31:53 +0530

    product crud complete

--- controllers/product.js
+++ controllers/product.js
@@ -14,7 +14,40 @@ exports.create = async(req,res) =>{
     }
 }
 
-exports.read = async(req,res)=>{
+exports.listWithCount = async(req,res)=>{
     let products = await Product.find({})
+    .limit(parseInt(req.params.count))
+    .populate('category')
+    .populate('subcategories')
+    .sort([['createdAt','desc']])
+    .exec()
     res.json(products)
+}
+
+exports.deleteProduct = async(req,res)=>{
+    try{
+        let deleted = await Product.findOneAndRemove({slug:req.params.slug})
+        .exec()
+        res.json(deleted)
+    }catch(err){
+        res.status(400).semd('Product Delete Failed')
+    }
+}
+
+exports.readProduct = async(req,res)=>{
+    const product = await Product.findOne({slug:req.params.slug})
+    .populate('category')
+    .populate('subcategories')
+    .exec()
+    res.json(product)
+}
+
+exports.update = async(req,res)=>{
+    try{
+        const updateProduct = await Product.findOneAndUpdate({slug:req.params.slug},req.body,{new:true}).exec()
+        res.json(updateProduct)
+    }catch(err){
+        console.log('UPDATING PRODUCT ERR',err)
+        return res.status(400).json({'err':'Product update failed'})
+    }
 }
--- routes/product.js
+++ routes/product.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router = express.Router()
-const {create,read} = require('../controllers/product')
+const {create,listWithCount,deleteProduct,readProduct,update} = require('../controllers/product')
 const {authCheck,adminCheck} = require('../middlewares/auth')
 
-router.get('/products',read)
+router.get('/products/:count',listWithCount)
+router.get('/product/:slug',readProduct)
 
 router.post('/product',authCheck,adminCheck,create)
+router.put('/product/:slug',authCheck,adminCheck,update)
+router.delete('/product/:slug',authCheck,adminCheck,deleteProduct)
 
 
 module.exports = router;
